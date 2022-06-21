2f5ab667aee09bb2cd01d3b0945db5cbda107afd
Виктор Михайлов <v.mikhailov@yottapay.co.uk>
2022-06-21 16:07:11 +0300

    add qr core payment url to payment creation response

--- src/main/java/com/yotta/sdk/core/service/YpObjectMapperService.java
+++ src/main/java/com/yotta/sdk/core/service/YpObjectMapperService.java
@@ -1,11 +1,12 @@
 package com.yotta.sdk.core.service;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yotta.sdk.core.service.impl.YpObjectMapperServiceImpl;
 
 public interface YpObjectMapperService {
 
-    YpObjectMapperService DEFAULT = new YpObjectMapperServiceImpl(new ObjectMapper());
+    YpObjectMapperService DEFAULT = new YpObjectMapperServiceImpl(new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
 
     String objectToJson(Object object);
 
